64bbb66cacfe9f248a3281f2efb0d0d43392580a
jasonhojh <jasonhojh1122@gmail.com>
2020-03-19 17:34:24 +0800

    update perlin

--- Perlin.hpp
+++ Perlin.hpp
@@ -6,8 +6,19 @@
 class Perlin {
 public:
     float noise(const glm::vec3& p) const {
-        
+        float u = p.x - floor(p.x);
+        float v = p.y - floor(p.y);
+        float w = p.z - floor(p.z);
+        int i = int(4*p.x) & 255;
+        int j = int(4*p.y) & 255;
+        int k = int(4*p.z) & 255;
+        return randFloat[permX[i] ^ permY[j] ^ permZ[k]];
     }
+
+    static float *randFloat;
+    static int *permX;
+    static int *permY;
+    static int *permZ;
 };
 
 static float* randomFloatSeires() {
@@ -17,3 +28,24 @@ static float* randomFloatSeires() {
     return p;
 }
 
+void permute(int *p, int n){
+    for (int i = n - 1; i > 0; --i){
+        int target = int(realRand()*(i+1));
+        int tmp = p[i];
+        p[i] = p[target];
+        p[target] = tmp;
+    }
+}
+
+static int* perlinPermute(){
+    int *p = new int[256];
+    for (int i = 0; i < 256; ++i)
+        p[i] = i;
+    permute(p, 256);
+    return p;        
+}
+
+float *Perlin::randFloat = randomFloatSeires();
+int *Perlin::permX = perlinPermute();
+int *Perlin::permY = perlinPermute();
+int *Perlin::permZ = perlinPermute();
